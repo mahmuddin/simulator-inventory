3f17cfeed458ae9badaaad7be88bf9e187c60cb2
Mahmuddin Nurul Fajri <mahmuddinnf@gmail.com>
2024-12-03 14:13:29 +0700

    add Tailwind

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+  user: String,
+});
+</script>
+<template>
+  <div>
+    <h1>About Page {{ user }}</h1>
+  </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home', []);
 });
+
+Route::inertia('/about', 'About', ['user' => 'Mike']);
